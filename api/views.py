308452bfb5b5c0422fa7db6e7411c819c818bf99
Minde-PC\Minde <mindaugas.kurmauskas@gmail.com>
2017-10-17 22:31:37 +0300

    Combine multiple raster images to one and return it

--- api/views.py
+++ api/views.py
@@ -14,8 +14,11 @@ import rasterio
 import rasterio.tools
 import rasterio.mask
 import rasterio.warp
+import rasterio.merge
 
-BASE_URL = ['https://sentinel-s2-l1c.s3.amazonaws.com', 'tiles']
+BASE_URL = ['https://sentinel-s2-l1c.s3.amazonaws.com']
+REQUEST_URL = BASE_URL.append('tiles')
+QUERY_URL = BASE_URL.appen('#tiles')
 
 @csrf_exempt
 def image_view(request):
@@ -36,42 +39,40 @@ def image_view(request):
         exact_tile = mgrs.MGRS().toMGRS(coordinate[1], coordinate[0]).decode('ascii')
         tiles.append(exact_tile[0:5])
 
-    if len(np.unique(tiles)) != 1:
-        return JsonResponse({
-            'error': 'not_supported',
-            'message': 'Not supported: Shapefile spans multiple MGRS tiles',
-        }, safe=False)
+    raster_files = []
+    for tile in tiles:
+        utm_code = tile[0:2]
+        latitude_band = tile[2:3]
+        square = tile[3:5]
 
-    tile = tiles[0]
+        TILE_PART = [utm_code, latitude_band, square]
+        DATE_PART = ['{dt.year}/{dt.month}/{dt.day}'.format(dt = parser.parse(data['date']))]
+        TAKE_PART = [data['take']]
+        BAND_PART = ['B' + data['band'] + '.jp2']
 
-    utm_code = tile[0:2]
-    latitude_band = tile[2:3]
-    square = tile[3:5]
+        url = '/'.join(np.concatenate((REQUEST_URL, TILE_PART, DATE_PART, TAKE_PART, BAND_PART)))
+        print('Requesting:', url)
 
-    TILE_PART = [utm_code, latitude_band, square]
-    DATE_PART = ['{dt.year}/{dt.month}/{dt.day}'.format(dt = parser.parse(data['date']))]
-    TAKE_PART = [data['take']]
-    BAND_PART = ['B' + data['band'] + '.jp2']
+        with rasterio.open(url) as raster_file:
+            raster_files.append(raster_file)
 
-    url = '/'.join(np.concatenate((BASE_URL, TILE_PART, DATE_PART, TAKE_PART, BAND_PART)))
-    print('Requesting:', url)
+    raster_file = rasterio.merge(raster_files)
 
-    with rasterio.open(url) as raster_file:
-        shape = geojson['features'][0]['geometry']
-        transformed_geojson = rasterio.warp.transform_geom('epsg:4326', raster_file.crs, shape)
-        out_image, out_transform = rasterio.mask.mask(raster_file, [transformed_geojson], crop=True)
+    shape = geojson['features'][0]['geometry']
+    transformed_geojson = rasterio.warp.transform_geom('epsg:4326', raster_file.crs, shape)
+    out_image, out_transform = rasterio.mask.mask(raster_file, [transformed_geojson], crop=True)
 
-        out_meta = raster_file.meta.copy()
+    out_meta = raster_file.meta.copy()
 
-        out_meta.update({
-            'driver': 'JP2OpenJPEG',
-             'height': out_image.shape[1],
-             'width': out_image.shape[2],
-             'transform': out_transform,
-         })
+    out_meta.update({
+        'driver': 'JP2OpenJPEG',
+         'height': out_image.shape[1],
+         'width': out_image.shape[2],
+         'transform': out_transform,
+     })
 
-        with rasterio.open('out.jp2', 'w', **out_meta) as dest:
-            dest.write(out_image)
+    with rasterio.open('out.jp2', 'w', **out_meta) as dest:
+        dest.write(out_image)
 
     fsock = open('out.jp2', 'rb')
     response = HttpResponse(fsock, content_type='image/jp2')
